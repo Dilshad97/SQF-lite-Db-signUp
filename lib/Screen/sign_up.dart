b8d31e66024de61e7d3602f46691d19708b34745
DILSHAD ALAM <rayeendilshan@gmail.com>
2021-11-17 18:31:49 +0530

    Add existing file

---
+++ lib/Screen/sign_up.dart
@@ -0,0 +1,164 @@
+import 'package:database/Database/db_helper.dart';
+import 'package:database/Database/model.dart';
+import 'package:database/widget/gettextfield.dart';
+import 'package:flutter/material.dart';
+import 'homepage.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({Key? key}) : super(key: key);
+
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  final _formKey = GlobalKey<FormState>();
+
+  TextEditingController firstname = TextEditingController();
+  TextEditingController lastname = TextEditingController();
+  TextEditingController mobile = TextEditingController();
+  TextEditingController password = TextEditingController();
+  TextEditingController confirmpassword = TextEditingController();
+  TextEditingController emailcotroller = TextEditingController();
+  var dbHelper;
+
+  @override
+  void initState() {
+    super.initState();
+    dbHelper = DbHelper();
+  }
+
+  // signUp() async {
+  //   String fname = firstname.text;
+  //   String lname = lastname.text;
+  //   String email = emailcotroller.text;
+  //   String passwd = password.text;
+  //   String cpasswd = confirmpassword.text;
+  //
+  //   if (_formKey.currentState!.validate()) {
+  //     if (passwd != cpasswd) {
+  //       alertDialog(context, 'Password Mismatch');
+  //     } else {
+  //       _formKey.currentState!.save();
+  //       UserModel uModel = UserModel(uid, uname, email, passwd);
+  //       await dbHelper.saveData(uModel).then((userData) {
+  //         alertDialog(context, "Successfully Saved");
+  //
+  //       Navigator.push(
+  //           context, MaterialPageRoute(builder: (_) => HomePage()));
+  //     }
+  //   }
+  // }
+
+  signUp() async {
+    String fname = firstname.text;
+    String lname = lastname.text;
+    String email = emailcotroller.text;
+    String passwd = password.text;
+    String cpasswd = confirmpassword.text;
+
+    if (_formKey.currentState!.validate()) {
+      if (passwd != cpasswd) {
+        alertDialog(context, 'Password Mismatch');
+      } else {
+        _formKey.currentState!.save();
+
+        UserModel uModel = UserModel(fname, lname, email, passwd);
+        await dbHelper.saveData(uModel).then((userData) {
+          alertDialog(context, "Successfully Saved");
+          Navigator.push(
+              context, MaterialPageRoute(builder: (_) => const HomePage()));
+        }).catchError((error) {
+          print(error);
+          alertDialog(context, "Error: Data Save Fail");
+        });
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        title: const Text('My Login'),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height / 4.5,
+                width: MediaQuery.of(context).size.width,
+                decoration: const BoxDecoration(color: Colors.blue),
+                child: const Icon(
+                  Icons.person,
+                  size: 150,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Textfield(
+                controller: firstname,
+                icon: Icons.person,
+                hintName: 'Enter First Name',
+                inputType: TextInputType.text,
+              ),
+              Textfield(
+                controller: lastname,
+                icon: Icons.person,
+                hintName: 'Enter Last Name',
+                inputType: TextInputType.text,
+              ),
+              Textfield(
+                controller: emailcotroller,
+                icon: Icons.mail,
+                hintName: 'Enter Email',
+                inputType: TextInputType.emailAddress,
+              ),
+              Textfield(
+                controller: password,
+                icon: Icons.lock,
+                hintName: 'Password',
+                inputType: TextInputType.visiblePassword,
+                isObscureText: true,
+              ),
+              Textfield(
+                controller: confirmpassword,
+                icon: Icons.lock,
+                hintName: 'Confirm Password',
+                inputType: TextInputType.visiblePassword,
+                isObscureText: true,
+              ),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      primary: Colors.deepOrangeAccent),
+                  onPressed: signUp,
+                  child: const Text('Sign In')),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Already have a account?'),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  GestureDetector(
+                    child: const Text(
+                      'Login ',
+                      style: TextStyle(color: Colors.blue, fontSize: 17),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+}
---
+++ lib/widget/gettextfield.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class Textfield extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintName;
+  final IconData icon;
+  final bool isObscureText;
+  final TextInputType inputType;
+  final bool isEnable;
+
+  Textfield(
+      {required this.controller,
+      required this.hintName,
+      required this.icon,
+      this.isObscureText = false,
+      this.inputType = TextInputType.text,
+      this.isEnable = true,
+      Key? key})
+      : super(key: key);
+
+  final TextEditingController password = TextEditingController();
+  final TextEditingController confirmpassword = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(left: 30, right: 30, top: 5, bottom: 5),
+      child: TextFormField(
+        controller: controller,
+        obscureText: isObscureText,
+        enabled: isEnable,
+        keyboardType: inputType,
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Please enter $hintName';
+          }
+          if (hintName == "Enter Email" && !validateEmail(value)) {
+            return 'Please enter valid email';
+          }
+          if (hintName == "Enter First Name" && hintName.length >= 50) {
+            return 'First Name cannt be exceed 0f 50 char';
+          }
+          if (hintName == "Enter Last Name" && hintName.length >= 100) {
+            return 'Last Name cannt be exceed of 100 char';
+          }
+          if (hintName == "Password" && hintName.length > 20) {
+            return 'Password should be under 20 char';
+          }
+
+          return null;
+        },
+        decoration: InputDecoration(
+          enabledBorder: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(30.0)),
+            borderSide: BorderSide(color: Colors.transparent),
+          ),
+          focusedBorder: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(30.0)),
+            borderSide: BorderSide(color: Colors.blue),
+          ),
+          prefixIcon: Icon(icon),
+          hintText: hintName,
+          labelText: hintName,
+          fillColor: Colors.grey[200],
+          filled: true,
+        ),
+      ),
+    );
+  }
+}
+
+alertDialog(BuildContext context, String msg) {
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Text(msg),
+        actions: [
+          ElevatedButton(
+            child: const Text('Ok'),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      );
+    },
+  );
+  // Toast.show(msg, context, duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+}
+
+validateEmail(String email) {
+  final emailReg = RegExp(
+      r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
+  return emailReg.hasMatch(email);
+}
